72802093aa1db50398cd22a361cfe334f91e1f44
batuhanbilginn <batuhanbilginn@gmail.com>
2023-04-07 23:51:37 +0300

    styling fixes for mobile - 2

--- app/(chat)/chat/page.tsx
+++ app/(chat)/chat/page.tsx
@@ -2,7 +2,7 @@ import Chatbox from "@/components/chat/chatbox";
 
 export default function Home() {
   return (
-    <div className="w-full">
+    <div className="w-full h-full">
       {/* Chatbox */}
       <Chatbox />
     </div>
--- components/chat/new-chat.tsx
+++ components/chat/new-chat.tsx
@@ -23,9 +23,9 @@ const NewChat = () => {
   const [systemPropmt, setSystemPropmt] = useAtom(focusedSystemPropmtAtom);
 
   return (
-    <div className="flex items-center justify-center w-full h-full">
+    <div className="flex items-start justify-center w-full h-screen sm:items-center">
       {/* Container */}
-      <div className="w-full px-8 py-10 mx-8 rounded-md shadow-sm md:max-w-xl dark:bg-neutral-950/30 bg-white/50">
+      <div className="w-full max-h-full px-8 py-10 mx-8 rounded-md shadow-sm md:max-w-xl dark:bg-neutral-950/30 bg-white/50">
         <h2 className="text-lg font-medium">Start a New Chat</h2>
         <p className="mt-1 font-light dark:text-neutral-500">
           Please select your model and system propmt first. Then, you can{" "}
