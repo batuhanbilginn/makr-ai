1a8afbf9f97ed90854602babab769d677dea79a1
batuhanbilginn <batuhanbilginn@gmail.com>
2023-04-12 15:46:47 +0300

    fix:redirect link on different env.

--- lib/supabase/supabase-auth-provider.tsx
+++ lib/supabase/supabase-auth-provider.tsx
@@ -65,6 +65,13 @@ export default function SupabaseAuthProvider({
   // Sign-In with Github
   const signInWithGithub = async () => {
     console.log(process.env.NEXT_PUBLIC_VERCEL_ENV);
+    console.log(
+      "Redirect to:" + process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+        ? "https://ai.makr.dev/chat"
+        : process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
+        ? "https://preview-ai.makr.dev/chat"
+        : "http://localhost:3000/chat"
+    );
     await supabase.auth.signInWithOAuth({
       provider: "github",
       options: {
